09f5f0ec3217653e3965ca5770d90d4ac880ded7
Chelsey Shao <ssh21927@gmail.com>
2023-06-13 18:45:36 +0800

    update cal_mr by consindering virtual borrwo

--- cr_monitor/position/positionOkex.py
+++ cr_monitor/position/positionOkex.py
@@ -9,7 +9,14 @@ class PositionOkex(object):
     def __init__(self) -> None:
         self.database = ConnectData()
         self.data_okex = ConnectOkex()
-        self.fee_rate = 0.001
+        self.fee_rate = 0.000225
+        self.spot_level = {"BTC": 7, "ETH": 7, "USDT": 5, "USDC": 5, "other": 1}
+        self.uswap_level = {"BTC": 20, "ETH": 20, "other": 10}
+        self.usdcswap_level = {"BTC": 10, "ETH": 5}
+        self.cswap_level = {"other": 10}
+        self.cfuture_level = {"other": 10}
+        self.ufuture_level = {"other": 10}
+        self.virtual_borrow: dict[str, float] = {}
         self.equity:dict[str, float] = {"BTC": 50} # the asset
         self.contracts = ["usdt", "usdt-swap", "usdt-future", "usd-swap", "usd-future", "usdc-swap"]
         self.now_position = pd.DataFrame(columns = self.contracts)
@@ -97,6 +104,7 @@ class PositionOkex(object):
                         upnl = self.now_position.loc[coin, contract] * (self.now_price.loc[coin, contract] - self.open_price.loc[coin, contract])
                         stable_coin = contract.split("-")[0].upper()
                         self.equity[stable_coin] = upnl if stable_coin not in self.equity.keys() else self.equity[stable_coin] + upnl
+                    self.upnl[coin] = upnl
     
     def get_adjEq(self):
         self.check_position_price()
@@ -104,7 +112,7 @@ class PositionOkex(object):
         self.get_disacount_equity()
     
     def get_mmr_contract(self):
-        self.mmr_liability = pd.DataFrame(index = self.now_position.index, columns = self.now_position.columns)
+        self.mmr_contract = pd.DataFrame(index = self.now_position.index, columns = self.now_position.columns)
         for coin in self.now_position.index:
             for contract in self.now_position.columns.drop("usdt"):
                 self.mmr_contract.loc[coin, contract] = self.data_okex.get_mmr(coin, self.now_position.loc[coin, contract], contract)
@@ -116,11 +124,43 @@ class PositionOkex(object):
                 self.position_value.loc[coin, contract] = abs(self.now_position.loc[coin, contract]) * self.now_price.loc[coin, contract] if contract.split("-")[0] != "usd" else\
                     abs(self.now_position.loc[coin, contract]) * self.data_okex.get_contractsize_cswap(coin)
     
+    def get_contract_level(self, coin: str, contract: str) -> float:
+        coin = coin.upper()
+        if contract == "usdt-swap":
+            ret = self.uswap_level[coin] if coin in self.uswap_level.keys() else self.uswap_level["other"]
+        elif contract == "usd-swap":
+            ret = self.cswap_level[coin] if coin in self.cswap_level.keys() else self.cswap_level["other"]
+        elif contract == "usdt-future":
+            ret = self.ufuture_level[coin] if coin in self.ufuture_level.keys() else self.ufuture_level["other"]
+        elif contract == "usd-future":
+            ret = self.cfuture_level[coin] if coin in self.cfuture_level.keys() else self.cfuture_level["other"]
+        else:
+            ret = np.nan
+        return ret
+    
+    def get_spot_level(self, coin: str, contract: str) -> tuple[str, float]:
+        coin = coin.upper() if contract.split("-")[0] == "usd" else contract.split("-")[0].upper()
+        level = self.spot_level[coin] if coin in self.spot_level.keys() else self.spot_level["other"]
+        return coin, level
+    
+    def get_position_frozen(self):
+        self.get_position_value() if not hasattr(self, "position_value") else None
+        self.virtual_borrow = {}
+        self.position_frozen: pd.DataFrame = pd.DataFrame(index = self.position_value.index, columns = self.now_position.columns.drop("usdt"))
+        for coin in self.position_frozen.index:
+            for contract in self.position_frozen.columns:
+                level = self.get_contract_level(coin, contract)
+                self.position_frozen.loc[coin, contract] = self.position_value.loc[coin, contract] / level
+                borrow_coin, spot_level = self.get_spot_level(coin, contract)
+                inUse = self.position_frozen.loc[coin, contract] / spot_level
+                self.virtual_borrow[borrow_coin] = inUse if borrow_coin not in self.virtual_borrow.keys() else self.virtual_borrow[borrow_coin] + inUse
+    
     def get_mm_liability(self):
         self.mm_liability, self.mmr_liability = {}, {}
         for coin, amount in self.equity.items():
-            self.mmr_liability[coin] = self.data_okex.get_mmr(coin, amount, contract = "spot")
-            self.mm_liability[coin] = self.mmr_liability[coin] * abs(amount) * self.get_coin_price(coin)
+            real_amount = amount - self.virtual_borrow[coin] if coin in self.virtual_borrow.keys() else amount
+            self.mmr_liability[coin] = self.data_okex.get_mmr(coin, real_amount, contract = "spot")
+            self.mm_liability[coin] = self.mmr_liability[coin] * abs(real_amount) * self.get_coin_price(coin)
     
     def get_mm(self):
         self.get_mmr_contract()
