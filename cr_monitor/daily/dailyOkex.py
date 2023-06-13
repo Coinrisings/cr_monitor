7906ca94fd774580f52d04787b9a80a8bb8b7c09
Chelsey Shao <ssh21927@gmail.com>
2023-06-13 19:06:14 +0800

    fix bug about get_account_mr add

--- cr_monitor/daily/dailyOkex.py
+++ cr_monitor/daily/dailyOkex.py
@@ -70,7 +70,7 @@ class DailyOkex(object):
         self.account_mr: dict[str, dict[str, float]] = {}
         for name, account in self.accounts.items():
             # self.mr_okex = MrOkex()
-            self.account_mr[name] = self.mr_okex.run_account_mr(account = account, add = {})
+            self.account_mr[name] = self.mr_okex.run_account_mr(account = account, add = add)
         ret = pd.DataFrame.from_dict(self.account_mr)
         ret = ret.style.applymap(set_color) if is_color else ret
         return ret
