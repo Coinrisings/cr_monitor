481085af44ea11a9b4298a5fce2ebfd79c6bac3e
Chelsey Shao <ssh21927@gmail.com>
2023-07-25 14:44:27 +0800

    update run_mr

--- cr_monitor/run/run_mr.py
+++ cr_monitor/run/run_mr.py
@@ -4,17 +4,17 @@ from cr_monitor.mr.mrOkex import MrOkex
 import pandas as pd
 import numpy as np
 import copy
-
-m = MrOkex()
-m.btc_num = [0.09]
-# m.price_range = [1]
 add = {
-    "okx_spot-okx_usdt_swap": {'ltc':0.5,'beth':0.5}
+    "okx_spot-okx_usdt_swap": {'eth':-1,'beth':1}
 }
+m = MrOkex()
+m.btc_num = [0.1]
+m.price_range = [1]
 result = m.assumed_open(add)
 result
+
 m = MrOkex()
-account = AccountOkex("test_otest8@pt_okex_btc")
+account = AccountOkex("test_otest7@pt_okex_btc")
 # m.price_range = [1]
 ret = m.run_account_mr(account)
 ret
