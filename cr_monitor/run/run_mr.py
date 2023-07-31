1e3a39f19464594f73ac9f5f0813f92f6f05af7e
Chelsey Shao <ssh21927@gmail.com>
2023-07-31 11:59:13 +0800

    update run_mr

--- cr_monitor/run/run_mr.py
+++ cr_monitor/run/run_mr.py
@@ -5,16 +5,21 @@ import pandas as pd
 import numpy as np
 import copy
 add = {
-    "okx_spot-okx_usdt_swap": {'eth':-1,'beth':1}
+    "okx_spot-okx_usdt_swap": {'blur':0.15,'ltc':0.1,'dydx':0.1,'cfx':0.1,'fil':0.1,'arb':0.1},
+    "okx_spot-okx_usd_swap":{},
+    "okx_usdc_swap-okx_spot":{},
+    "okx_usd_swap-okx_usdt_swap":{'xrp':0.1},
+    "okx_usd_swap-okx_usdc_swap":{},
+    "okx_usdc_swap-okx_usdt_swap":{}
 }
-m = MrOkex()
-m.btc_num = [0.1]
-m.price_range = [1]
-result = m.assumed_open(add)
-result
+# m = MrOkex()
+# m.btc_num = [0.1]
+# m.price_range = [1]
+# result = m.assumed_open(add)
+# result
 
 m = MrOkex()
-account = AccountOkex("test_otest7@pt_okex_btc")
+account = AccountOkex("colin_001@pt_okex_btc")
 # m.price_range = [1]
-ret = m.run_account_mr(account)
+ret = m.run_account_mr(account, add = add)
 ret
