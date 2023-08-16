ab5253e0ca96d24e260aa323adce2e82eb193f1e
Chelsey Shao <ssh21927@gmail.com>
2023-08-16 16:14:02 +0800

    update bug abount btc dt value

--- cr_monitor/position/positionOkex.py
+++ cr_monitor/position/positionOkex.py
@@ -42,7 +42,7 @@ class PositionOkex(object):
         for coin in self.now_position[self.now_position["usdt"] != 0].index:
             self.equity[coin] = self.equity[coin] + self.now_position.loc[coin, "usdt"] if coin in self.equity.keys() else self.now_position.loc[coin, "usdt"]
             open_price = self.open_price.loc[coin, "usdt"] if coin in self.open_price.index else np.nan
-            self.equity["USDT"] -= self.now_position.loc[coin, "usdt"] * open_price if coin != "BTC" else - sum(self.now_position.loc[coin].drop("usdt")) * open_price
+            self.equity["USDT"] -= self.now_position.loc[coin, "usdt"] * open_price if coin != "BTC" else - (sum(self.now_position.loc[coin].drop(["usdt", "usd-swap", "usd-future"])) * open_price + self.now_position.loc[coin, ["usd-swap", "usd-future"]].sum()*self.data_okex.get_contractsize_cswap(coin))
         self.get_upnl()
         
     def get_discount_asset(self, coin: str, asset: float) ->float:
